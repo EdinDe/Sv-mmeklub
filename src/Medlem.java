c6387bf81d35d5ca81741381c8c38f1e9a3c349b
Salem <salo0002@stud.kea.dk>
2024-05-01 13:00:09 +0200

    nyny

--- src/Medlem.java
+++
@@ -1,9 +0,0 @@
-public class Medlem {
-
-        String navn;
-        String køn;
-        int fødselsdato;
-        int telefonnumer;
-        String medlemstype;
-
-}
--- src/Medlemmer.java
+++ src/Medlemmer.java
@@ -1,2 +1,76 @@
 public class Medlemmer {
+
+    String navn;
+    String køn;
+    int fødselsDato;
+    int telefonNummer;
+    String medlemsType;
+    boolean juniorEllerSenior;
+    boolean motionistEllerKonku;
+
+    public Medlemmer(String navn, String køn, int fødselsDato, int telefonNummer, String medlemsType, boolean juniorEllerSenior, boolean mosionistEllerKonku) {
+        this.navn = navn;
+        this.køn = køn;
+        this.fødselsDato = fødselsDato;
+        this.telefonNummer = telefonNummer;
+        this.medlemsType = medlemsType;
+        this.juniorEllerSenior = juniorEllerSenior;
+        this.motionistEllerKonku = mosionistEllerKonku;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public String getKøn() {
+        return køn;
+    }
+
+    public void setKøn(String køn) {
+        this.køn = køn;
+    }
+
+    public int getFødselsDato() {
+        return fødselsDato;
+    }
+
+    public void setFødselsDato(int fødselsDato) {
+        this.fødselsDato = fødselsDato;
+    }
+
+    public int getTelefonNummer() {
+        return telefonNummer;
+    }
+
+    public void setTelefonNummer(int telefonNummer) {
+        this.telefonNummer = telefonNummer;
+    }
+
+    public String getMedlemsType() {
+        return medlemsType;
+    }
+
+    public void setMedlemsType(String medlemsType) {
+        this.medlemsType = medlemsType;
+    }
+
+    public boolean isJuniorEllerSenior() {
+        return juniorEllerSenior;
+    }
+
+    public void setJuniorEllerSenior(boolean juniorEllerSenior) {
+        this.juniorEllerSenior = juniorEllerSenior;
+    }
+
+    public boolean isMotionistEllerKonku() {
+        return motionistEllerKonku;
+    }
+
+    public void setMotionistEllerKonku(boolean motionistEllerKonku) {
+        this.motionistEllerKonku = motionistEllerKonku;
+    }
 }
