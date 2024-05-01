5289b8c49f0cdc81a991c31be4fcef98b547d25f
Salem <salo0002@stud.kea.dk>
2024-05-01 13:00:09 +0200

    y

--- src/Medlem.java
+++ src/Medlem.java
@@ -1,9 +1,20 @@
-public class Medlem {
+        public class Medlem {
 
         String navn;
         String køn;
         int fødselsdato;
         int telefonnumer;
         String medlemstype;
+
+}
+
+        public Medlem(String navn, String køn, int fødselsdato, int telefonnummer, String medlemstype){
+
+        this.navn=navn;
+        this.køn=køn;
+        this.fødselsdato=fødselsdato;
+        this.telefonnumer=telefonnummer;
+        this.medlemstype
+
 
 }
