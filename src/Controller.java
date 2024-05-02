5a05b5e97aaa901ce5ce72efdffc1895d553515f
samiahamzi <saah0001@stud.kea.dk>
2024-05-02 13:09:59 +0200

    e

--- src/Controller.java
+++ src/Controller.java
@@ -2,9 +2,12 @@ public class Controller {
 
     public static Medlemsliste nyMedlemsListe = new Medlemsliste();
 
-    public Controller(){
+    public Controller() {
 
     }
 
-    public static void addMedlem
+
+    public static void addMedlem(String navn, String køn, int fødselsdato, int telefonNummer, String medlemstype, boolean juniorEllerSenior, boolean motionistEllerKonku) {
+        nyMedlemsListe.addMedlem(navn, køn, fødselsdato, telefonNummer, medlemstype, juniorEllerSenior, motionistEllerKonku);
+    }
 }
